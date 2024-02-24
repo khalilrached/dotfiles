fcbe72dbf03ab87d855f3c2c0d47a55724a2e24d
khalilrached <khalilrached058@gmail.com>
2024-02-24 23:15:38 +0100

    laptop config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+alias ll "ls -lha"
+
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.config/composer/vendor/bin"
