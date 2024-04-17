3ba1432d0ce7dd4d5ae7b71096444342d29b19c1
khalilrached <khalilrached058@gmail.com>
2024-04-17 02:00:16 +0100

    desktop: update nvm

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,20 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    	# Commands to run in interactive sessions can go here
+	# Compilation flags
+	# export ARCHFLAGS="-arch x86_64"
+	export LIBVA_DRIVER_NAME="vdpau"
+	export VDPAU_DRIVER="nvidia"
+	export JAVA_HOME="/usr/lib/jvm/java-21-openjdk/"
+	#Set personal aliases, overriding those provided by oh-my-zsh libs,
+	# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+	# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+	# For a full list of active aliases, run `alias`.
+	#
+	# Example aliases
+	# alias zshconfig="mate ~/.zshrc"
+	# alias ohmyzsh="mate ~/.oh-my-zsh"
+	alias ll="ls -lha"
+
+	alias enable_idf="source /usr/local/lib/esp/esp-idf/export.sh > /dev/null"
+
 end
